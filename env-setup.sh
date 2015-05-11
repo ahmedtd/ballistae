15ac0a96a24a94858cf0636145b54aab6061c3cc
Taahir Ahmed <ahmed.taahir@gmail.com>
2015-05-11 17:05:41 -0500

    A useful environment setup script.

---
+++ env-setup.sh
@@ -0,0 +1,4 @@
+export LIBRARY_PATH=$LIBRARY_PATH:${fakeusr}/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${fakeusr}/lib
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${fakeusr}/include
+export GUILE_LOAD_PATH=$GUILE_LOAD_PATH:${fakeusr}/share/guile/2.0
