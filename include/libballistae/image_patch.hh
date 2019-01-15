02c393de7d4e8a516597de57d3c1addaccb6b3f6
Taahir Ahmed <ahmed.taahir@gmail.com>
2019-01-15 00:13:59 -0800

    Partial tetrahedron support

---
+++ include/libballistae/image_patch.hh
@@ -0,0 +1,22 @@
+#ifndef LIBBALLISTAE_IMAGE_PATCH_HH
+#define LIBBALLISTAE_IMAGE_PATCH_HH
+
+#include <vector>
+
+namespace balliste {
+
+// Instead of keeping the actual basis vectors, just keep their
+// indices in the lexical ordering of all basis vectors.
+
+template<class Elem>
+struct image_patch {
+    // Cosine basis representation
+    std::size_t n_row_components;
+    std::size_t n_col_components;
+    std::size_t n_frq_components;
+    std::vector<Elem> basis_vectors;
+};
+
+}
+
+#endif
